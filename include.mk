0cd29eeec2acc2a2bc49c22ab23bef4261f39d68
jenbukow <130695354+jenbukow@users.noreply.github.com>
2024-03-08 14:49:24 -0700

    Resolve merge conflicts between INCUS prod and RAMS V6.3.04

--- include.mk
+++ include.mk
@@ -1,3 +1,17 @@
+# NOTES:
+# For doing parallel simulations, compile MPICH or OPENMPI with both FORTRAN
+# and C bindings turned on like the following configure statement for
+# compiling MPICH. Most of the time you enter the software package main
+# directory, issue the "configure" command, run "make", and then "make install".
+# For the configure statement to work, your ".bash_profile" needs to know "which"
+# C and Fortran compilers you are using. After compiling MPICH in this example,
+# it will create "mpif90" and "mpicc" that you can use in this include.mk file
+# for "F_COMP" and "C_COMP" env variables below. Make sure the paths to these
+# compilers and associated libraries are in your .bash_profile PATH and
+# LD_LIBRARY_PATH as needed.
+# ./configure --prefix=/file-path-here-for-mpi/mpich-3.3.2 \
+#  --enable-fast=O3 CPPFLAGS=-DNDEBUG CC=gcc FC=ifort
+
 #############################################################################
 # Define make (gnu make works best).
 #############################################################################
@@ -6,8 +20,8 @@ MAKE=/usr/bin/make
 #############################################################################
 # Set your RAMS root path and version number.
 #############################################################################
-RAMS_ROOT=/home/smsaleeb/rams_20210806_dev
-RAMS_VERSION=6.3.02
+RAMS_ROOT=/home/smsaleeb/rams_git_dev
+RAMS_VERSION=6.3.04
 
 #############################################################################
 # Set root locations for HDF5 I/O software.
@@ -23,12 +37,12 @@ H5Z_ZFP_ROOT=/home/pmarin/misc/h5z_zfp_git/H5Z-ZFP/install # For ZFP Compression
 # Set root locations for parallel processing MPI software.
 # You can comment out MPI_ROOT for serial processing compile.
 #############################################################################
-MPI_ROOT=/home/smsaleeb/software/mpich-3.3.2
+MPI_ROOT=#/home/smsaleeb/software/mpich-3.3.2
 
 #############################################################################
 # Do not change these 2. They point from RAMS_ROOT to the source code.
 #############################################################################
-MODEL=$(RAMS_ROOT)/src/$(RAMS_VERSION)
+MODEL=$(RAMS_ROOT)/src
 UTILS_INCS=-I$(MODEL)/include
 
 #############################################################################
@@ -80,11 +94,41 @@ CMACH=PC_LINUX1  #Standard Linux (only option available now)
 # (-g) for debugging, (-traceback) for more compiler error info
 # (-check bounds) for array bounds checking, (-fp-model precise) for IEEE
 # (-check uninit) for finding uninitialized variables, (-free) for free format
-F_COMP=/home/smsaleeb/intel/composer_xe_2011_sp1.8.273/bin/intel64/ifort
-F_OPTS1=-free -O1 -fp-model precise
-F_OPTS2=-free -O2 -fp-model precise
-LOADER_OPTS= -free -O2 -fp-model precise
-LIBS=-L/usr/lib/x86_64-linux-gnu -lrt -lpthread -lsz -lz
+#F_COMP=/home/smsaleeb/intel/composer_xe_2011_sp1.8.273/bin/intel64/ifort
+#F_COMP=/home/smsaleeb/software/mpich-3.3.2/bin/mpif90
+F_COMP=mpif90
+
+# Options going from fastest to slowest -- but slowests produce exact results regardless of system
+
+# Basic
+#F_OPTS1=-free -O1 -fp-model precise
+#F_OPTS2=-free -O2 -fp-model precise
+#LOADER_OPTS= -free -O2 -fp-model precise
+#LIBS=-L/usr/lib/x86_64-linux-gnu -L/nasa/szip/2.1.1/lib -lrt -lpthread -lsz -lz
+
+# Strict
+#F_OPTS1=-free -O1 -fp-model strict
+#F_OPTS2=-free -O2 -fp-model strict
+#LOADER_OPTS= -free -O2 -fp-model strict
+#LIBS=-L/usr/lib/x86_64-linux-gnu -L/nasa/szip/2.1.1/lib -lrt -lpthread -lsz -lz
+
+# All Intel - will produce idential results on all intel sytems
+#F_OPTS1=-free -O1 -fp-model strict -ipo -axCORE-AVX512,CORE-AVX2 -xAVX -qopt-zmm-usage=high
+#F_OPTS2=-free -O2 -fp-model strict -ipo -axCORE-AVX512,CORE-AVX2 -xAVX -qopt-zmm-usage=high
+#LOADER_OPTS= -free -O2 -fp-model strict -ipo -axCORE-AVX512,CORE-AVX2 -xAVX -qopt-zmm-usage=high
+#LIBS=-L/usr/lib/x86_64-linux-gnu -lrt -lpthread -lsz -lz -L/nasa/szip/2.1.1/lib
+
+# AMD Compatible Intel -- will produce the exact same results as AMD Rome, but will not work on AMD Rome due to 
+# AVX512, which does not work on AMD Rome (but should work on newer AMD systems)
+#F_OPTS1=-free -O1 -fp-model strict -ipo -axCORE-AVX512,CORE-AVX2 -xAVX -qopt-zmm-usage=high -fimf-arch-consistency=true
+#F_OPTS2=-free -O2 -fp-model strict -ipo -axCORE-AVX512,CORE-AVX2 -xAVX -qopt-zmm-usage=high -fimf-arch-consistency=true
+#LOADER_OPTS= -free -O2 -fp-model strict -ipo -axCORE-AVX512,CORE-AVX2 -xAVX -qopt-zmm-usage=high -fimf-arch-consistency=true
+#LIBS=-L/usr/lib/x86_64-linux-gnu -lrt -lpthread -lsz -lz -L/nasa/szip/2.1.1/lib
+
+F_OPTS1=-free -O1 -fp-model strict -ipo -march=core-avx2 -qopt-zmm-usage=high -fimf-arch-consistency=true #-real-size 64
+F_OPTS2=-free -O2 -fp-model strict -ipo -march=core-avx2 -qopt-zmm-usage=high -fimf-arch-consistency=true #-real-size 64
+LOADER_OPTS=-free -O2 -fp-model strict -ipo -march=core-avx2 -qopt-zmm-usage=high -fimf-arch-consistency=true #-real-size 64
+LIBS=-L/lib64 -lrt -lpthread -lsz -lz -L/nasa/szip/2.1.1/lib
 
 #*****************************
 # FORTRAN INTEL IFORT COMPILER Double Precision
@@ -126,7 +170,7 @@ LIBS=-L/usr/lib/x86_64-linux-gnu -lrt -lpthread -lsz -lz
 #############################################################################
 # C compiler choice and flags (gcc) and (mpicc) are most common
 # Add the "-DRAMS_DOUBLE_PREC" compiler flag to "C_OPTS" for turning on
-# double precision rather than default double precision. Note that using
+# double precision rather than default single precision. Note that using
 # double will make output and runtimes substantially longer but provide the
 # extra precision needed in some highly sensitive simulations.
 #
@@ -146,7 +190,7 @@ LIBS=-L/usr/lib/x86_64-linux-gnu -lrt -lpthread -lsz -lz
 # that are not really as issue for us, but you can turn warnings back on by
 # removing the "-w" if you wish to alter code to eliminate warnings.
 #############################################################################
-C_COMP=gcc
+C_COMP=mpicc
 C_OPTS=-O3 -DUNDERSCORE -DLITTLE -std=gnu99 -DENABLE_PARALLEL_COMPRESSION -w -DENABLE_ZFP_COMPRESSION
 #C_OPTS=-O3 -DUNDERSCORE -DLITTLE -std=gnu99 -DRAMS_DOUBLE_PREC \
 #  -DENABLE_PARALLEL_COMPRESSION -w
@@ -166,9 +210,10 @@ ARCH=ar rsU
 #  software and HDF5 software.
 # Add or remove needed libraries to PAR_LIBS. Missing or needed libraries
 #  will be highlighted in compile time error messages. Typical library
-#  specs are: -lmpich, -lmpl, -lmpi.
+#  specs are: -lmpich, -lmpl, -lmpi, -lmpifort
 # Comment out these "PAR_" lines for serial processing compile.
 #############################################################################
-PAR_INCS=-I$(MPI_ROOT)/include
-PAR_LIBS=-L$(MPI_ROOT)/lib -lmpich -lmpl
+#PAR_INCS=-I$(MPI_ROOT)/include
+#PAR_LIBS=-L$(MPI_ROOT)/lib -lmpich -lmpl
+#PAR_LIBS=-L$(MPI_ROOT)/lib
 PAR_DEFS=-DRAMS_MPI
